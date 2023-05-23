b4229e0108be6d8a18c5a3c7dbd587d91d3bd1e5
benisrlm <132856101+benisrlm@users.noreply.github.com>
2023-05-22 20:50:11 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 let reading = 0
+input.onButtonPressed(Button.A, function () {
+	
+})
 basic.forever(function () {
+    let if_the_variable_to_0 = 0
     reading = pins.digitalReadPin(DigitalPin.P0)
     led.plotBarGraph(
     pins.digitalReadPin(DigitalPin.P0),
@@ -7,5 +11,19 @@ basic.forever(function () {
     )
     if (input.buttonIsPressed(Button.A)) {
         basic.showNumber(reading)
+    } else {
+        basic.showNumber(if_the_variable_to_0)
+        servos.P0.setRange(0, 180)
+    }
+    reading = pins.digitalReadPin(DigitalPin.P1)
+    led.plotBarGraph(
+    pins.digitalReadPin(DigitalPin.P1),
+    1023
+    )
+    if (true) {
+        basic.showNumber(reading)
+    } else {
+        basic.showNumber(if_the_variable_to_0)
+        basic.showString("i need water")
     }
 })
