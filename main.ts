4df292a5d3b7c5f2c5b278c77ab560fe409c1069
benisrlm <132856101+benisrlm@users.noreply.github.com>
2023-05-25 19:12:37 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,13 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
 let reading = 0
 servos.P0.setAngle(115)
 basic.forever(function () {
+    basic.showString("R")
     basic.pause(500)
     reading = pins.analogReadPin(AnalogPin.P1)
     serial.writeNumber(reading)
     serial.writeLine("")
     if (450 < reading && reading < 490) {
+        basic.showString("L")
         serial.writeLine("launch")
         servos.P0.setAngle(50)
         basic.pause(2000)
